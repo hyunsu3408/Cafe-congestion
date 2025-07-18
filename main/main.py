54167b3e12dd1d30bfac4c2c7bb2ac3c9eceae3a
hyunsu3408 <hyunsu3408@gmail.com>
2025-07-18 23:29:07 +0900

    flask

--- main/main.py
+++ main/main.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 import psycopg2
 from db_connect import db_connect
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='web')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/results',method=['POST'])
+@app.route('/results',methods=['POST'])
 def results():
     region = request.form['region']
     conn, cur = db_connect()
@@ -21,5 +21,5 @@ def results():
     cafes= cur.fetchall()
     return render_template('results.html', cafes=cafes,region=region)
 
-if __name__ == 'main':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True,port=3000)
