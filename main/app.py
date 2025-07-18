d76974b4d031896ddbe0bfc4e210a75c7e8c0e76
hyunsu3408 <hyunsu3408@gmail.com>
2025-07-18 22:57:06 +0900

    flask

---
+++ main/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import psycopg2
+from db_connect import db_connect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results',method=['POST'])
+def results():
+    region = request.form['region']
+    conn, cur = db_connect()
+    cur.execute("""
+        SELECT * FROM cafe
+        WHERE address Like %s
+        ORDER BY review_count DESC
+        LIMIT 20
+                """,(f'%{region}%'))
+    cafes= cur.fetchall()
+    return render_template('results.html', cafes=cafes,region=region)
+
+if __name__ == 'main':
+    app.run(debug=True)
